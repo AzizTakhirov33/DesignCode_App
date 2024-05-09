e081be82541090780cf4f476f26e3b35fc26832c
AzizTakhirov33 <azizbektakhirov56@gmail.com>
2024-05-09 20:24:11 +0500

    Initial Commit

---
+++ DesignCode/DesignCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeApp.swift
+//  DesignCode
+//
+//  Created by Azizbek Tahirov on 09/05/24.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
